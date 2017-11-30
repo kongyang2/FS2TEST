854ad66a94e9ddd6184582fd219e5aa185014aee
flybaby-joycity <flybaby@joycity.com>
2017-11-30 06:52:03 +0000

    fix

--- transfer.py
+++ transfer.py
@@ -17,25 +17,30 @@ def read_from_sql(from_date, to_date):
     WinCntTotal,Amount,CreateDate from dbo.DailyUser where StatDate >= '%s' and StatDate <= '%s'" % (from_date, to_date)
     
     cursor.execute(query)
-    
+    count = 0; 
     for row in cursor:
         
         query = "INSERT INTO daily_user (user_id, date, amount, conn_time, play_count, play_time, win_count, create_date)\
-        values ('%s','%s',%d,%d,%d,%d,%d)" % (row['UserSN'], row['StatDate'], row['Amount'], row['ConnTime'], row['PlayCntTotal'], row['PlayTime'], row['WinCntTotal'], row['CreateDate'])
+        values ('%s','%s',%d,%d,%d,%d,%d,'%s')" % (row['UserSN'], row['StatDate'], row['Amount'], row['ConnTime'], row['PlayCntTotal'], row['PlayTime'], row['WinCntTotal'], row['CreateDate'])
         
        # print(query)
         session.execute(query)
+        count+=1;
+        if count % 100 == 0:
+            print('from sql %d fetched' % count , end='\r')
 
  
 def move_to_daily_user_2():
     
     rows = session.execute("SELECT user_id , date , amount , conn_time, play_count , play_time , win_count , create_date FROM daily_user");
-    
+    count = 0;
     for r in rows:
-        session.execute("INSERT INTO daily_user_2 (user_id,date,amount,conn_time,play_count,play_time,win_count, create_date , is_applied) VALUES ('%s','%s',%d,%d,%d,%d,%d,%s,%s)"
+        session.execute("INSERT INTO daily_user_2 (user_id,date,amount,conn_time,play_count,play_time,win_count, create_date , is_applied) VALUES ('%s','%s',%d,%d,%d,%d,%d,'%s',%s)"
                         % (r.user_id , r.date , r.amount , r.conn_time, r.play_count , r.play_time, r.win_count , r.create_date, False));
-       
-
+        count+=1;
+        if count % 100 == 0:
+            print('from daily_user %d fetched' % count , end='\r')
+            
 read_from_sql('2016-01-01', '2017-11-20')
 move_to_daily_user_2();
 
